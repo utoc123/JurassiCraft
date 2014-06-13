84b2e5e9d1727043dbd47c61a77654fbf7337574
BobMowzie <BobMowzie@users.noreply.github.com>
2014-06-13 17:47:12 -0400

    Update MowzieModelBase.java

--- src/main/java/com/ilexiconn/jurassicraft/data/entity/model/MowzieModelBase.java
+++ src/main/java/com/ilexiconn/jurassicraft/data/entity/model/MowzieModelBase.java
@@ -7,6 +7,8 @@ import net.minecraft.util.MathHelper;
 @SideOnly(Side.CLIENT)
 
 public class MowzieModelBase extends ModelBase {
+	
+	//Attach child to parent boxes to make the child move to match parent rotation and translation. Use this on the ends of parenting chains first. (I.E. Attach snout to head before attaching head to neck.)
 	protected void addChildTo(ModelRenderer child, ModelRenderer parent) {
 		float distance = (float)Math.sqrt(Math.pow((child.rotationPointZ - parent.rotationPointZ), 2) + Math.pow((child.rotationPointY - parent.rotationPointY), 2));
 		float oldRotateAngleX = parent.rotateAngleX;
@@ -23,6 +25,7 @@ public class MowzieModelBase extends ModelBase {
 	  	child.rotateAngleZ -= parent.rotateAngleZ;
 	 }
 	
+	//Makes the specified box face the mob's target. Leave "f3" and "f4" as "f3" and "f4". Divider specifies the number of parts that the box is rotating with. (I.E. Set to 3 for 3-segmented necks, 2 for 2, etc.)
 	public void faceTarget(MowzieModelRenderer box, int divider, float f3, float f4) {
 		box.rotateAngleY = (f3 / (180F / (float)Math.PI))/divider + box.initRotateAngleY;
 		box.rotateAngleX = (f4 / (180F / (float)Math.PI))/divider + box.initRotateAngleX;
