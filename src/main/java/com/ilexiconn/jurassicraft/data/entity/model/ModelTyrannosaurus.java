777765340bb65046cb812c1ad4e12dff20485a7e
BobMowzie <BobMowzie@users.noreply.github.com>
2014-06-13 17:39:27 -0400

    Whoops forgot imports

--- src/main/java/com/ilexiconn/jurassicraft/data/entity/model/ModelTyrannosaurus.java
+++ src/main/java/com/ilexiconn/jurassicraft/data/entity/model/ModelTyrannosaurus.java
@@ -2,8 +2,8 @@ package com.ilexiconn.jurassicraft.data.entity.model;
 
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
-import net.minecraft.client.model.ModelBase;
-import net.minecraft.client.model.ModelRenderer;
+import net.minecraft.client.model.MowzieModelBase;
+import net.minecraft.client.model.MowzieModelRenderer;
 import net.minecraft.entity.Entity;
 import org.lwjgl.opengl.GL11;
 
