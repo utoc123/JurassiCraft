17deb3045719dc05ba0cc3db7651efb9b9163d4d
TheLarsinator <TheLarsinatormodding@hotmail.com>
2014-06-15 20:08:43 +0200

    Dimetrodon sitting model!
    Sweet! Look how cute!

--- src/main/java/com/ilexiconn/jurassicraft/data/entity/EntityDimetrodon.java
+++ src/main/java/com/ilexiconn/jurassicraft/data/entity/EntityDimetrodon.java
@@ -76,7 +76,7 @@ public class EntityDimetrodon extends EntityTameable
         this.setTamed(false);
     }
 
-    protected void applyEntityAttributes()
+	protected void applyEntityAttributes()
     {
         super.applyEntityAttributes();
         this.getEntityAttribute(SharedMonsterAttributes.movementSpeed).setBaseValue(0.30000001192092896D);
