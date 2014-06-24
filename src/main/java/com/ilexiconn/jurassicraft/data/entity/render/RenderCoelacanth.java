4dc675d8013cfc7791c851eae34a0823ea941216
iLexiconn <ilexiconn@gmail.com>
2014-06-24 16:59:32 +0200

    Fixed this render file (too lazy to type the dino name here)

--- src/main/java/com/ilexiconn/jurassicraft/data/entity/render/RenderCoelacanth.java
+++ src/main/java/com/ilexiconn/jurassicraft/data/entity/render/RenderCoelacanth.java
@@ -1,17 +1,15 @@
 package com.ilexiconn.jurassicraft.data.entity.render;
 
-import net.minecraft.client.renderer.entity.RenderLiving;
+import com.ilexiconn.jurassicraft.Util;
+import com.ilexiconn.jurassicraft.data.entity.RenderDinosaurWaterMonster;
+import com.ilexiconn.jurassicraft.data.entity.model.ModelCoelacanth;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.entity.Entity;
 import net.minecraft.util.ResourceLocation;
 
-import com.ilexiconn.jurassicraft.Util;
-import com.ilexiconn.jurassicraft.data.entity.model.ModelCoelacanth;
-
-import cpw.mods.fml.relauncher.Side;
-import cpw.mods.fml.relauncher.SideOnly;
-
 @SideOnly(Side.CLIENT)
-public class RenderCoelacanth extends RenderLiving
+public class RenderCoelacanth extends RenderDinosaurWaterMonster
 {
     public RenderCoelacanth()
     {
