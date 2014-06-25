c623896f7626fb0ad72bbee9b00f6f853268261f
iLexiconn <ilexiconn@gmail.com>
2014-06-25 16:49:38 +0200

    Git Derp

--- src/main/java/com/ilexiconn/jurassicraft/proxy/ClientProxy.java
+++ src/main/java/com/ilexiconn/jurassicraft/proxy/ClientProxy.java
@@ -1,9 +1,5 @@
 package com.ilexiconn.jurassicraft.proxy;
 
-import com.ilexiconn.jurassicraft.Util;
-import com.ilexiconn.jurassicraft.data.block.BlockEgg;
-import com.ilexiconn.jurassicraft.data.tile.render.CultivateRenderer3D;
-import com.ilexiconn.jurassicraft.data.tile.render.EggRenderer3D;
 import cpw.mods.fml.client.registry.ClientRegistry;
 import cpw.mods.fml.client.registry.RenderingRegistry;
 import net.minecraft.client.renderer.entity.RenderLiving;
@@ -11,6 +7,7 @@ import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
 import net.minecraft.entity.EntityLiving;
 import net.minecraft.item.Item;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraftforge.client.IItemRenderer;
 import net.minecraftforge.client.MinecraftForgeClient;
 
 public class ClientProxy extends ServerProxy
@@ -25,12 +22,8 @@ public class ClientProxy extends ServerProxy
         ClientRegistry.bindTileEntitySpecialRenderer(tileEntity, renderer);
     }
 
-    public void renderItems()
+    public void renderItems(Item item, IItemRenderer render)
     {
-        MinecraftForgeClient.registerItemRenderer(Item.getItemFromBlock(Util.getBlock(1)), new CultivateRenderer3D());
-        for (BlockEgg egg : Util.getEggArray())
-        {
-            MinecraftForgeClient.registerItemRenderer(Item.getItemFromBlock(egg), new EggRenderer3D(egg.getDinoName()));
-        }
+        MinecraftForgeClient.registerItemRenderer(item, render);
     }
 }
