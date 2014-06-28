9791f173d0eff72adc0aaaaf99f071834c79f552
TheLarsinator <TheLarsinatormodding@hotmail.com>
2014-06-28 14:53:53 +0200

    Revert "Revert "FISHY!""
    This reverts commit 4feb39aa18596926cc26f77b43721d10e5c8456e.

---
+++ src/main/java/com/ilexiconn/jurassicraft/data/entity/render/RenderCoelacanth.java
@@ -0,0 +1,24 @@
+package com.ilexiconn.jurassicraft.data.entity.render;
+
+import com.ilexiconn.jurassicraft.JurassiCraft;
+import com.ilexiconn.jurassicraft.data.entity.Dinosaur;
+import com.ilexiconn.jurassicraft.data.entity.RenderDinosaur;
+import com.ilexiconn.jurassicraft.data.entity.model.ModelCoelacanth;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import net.minecraft.entity.Entity;
+import net.minecraft.util.ResourceLocation;
+
+@SideOnly(Side.CLIENT)
+public class RenderCoelacanth extends RenderDinosaur
+{
+    public RenderCoelacanth(Dinosaur dino)
+    {
+        super(new ModelCoelacanth(), dino, 1f);
+    }
+
+    public ResourceLocation getEntityTexture(Entity var1)
+    {
+        return new ResourceLocation(JurassiCraft.getModId() + "textures/entity/coelacanth1.png");
+    }
+}
