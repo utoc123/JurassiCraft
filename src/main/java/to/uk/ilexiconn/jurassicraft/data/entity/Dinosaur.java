1a7d18956f9edbd762bf3d83f896eddfffd6c839
iLexiconn <ilexiconn@gmail.com>
2014-09-21 07:56:56 +0200

    All dinos are broken now, kash fix them

--- src/main/java/to/uk/ilexiconn/jurassicraft/data/entity/Dinosaur.java
+++ src/main/java/to/uk/ilexiconn/jurassicraft/data/entity/Dinosaur.java
@@ -1,18 +1,43 @@
 package to.uk.ilexiconn.jurassicraft.data.entity;
 
+import java.util.ArrayList;
+
 public class Dinosaur
 {
-    public int id;
-    public String name;
-    public double speed;
-    public double health;
-    public double strength;
-    public float scale;
-    public float xzHitbox;
-    public float yHitbox;
-    public String livingSound1;
-    public String livingSound2;
-    public String hurtSound;
-    public String deathSound;
-    public boolean waterCreature;
+    public byte creatureID;
+
+    public String creatureName;
+
+    public double minHealth;
+    public double minStrength;
+    public double minSpeed;
+    public double minLength;
+    public double minHeight;
+    public double minKnockback;
+    public double minProximate;
+    public double minMinerals;
+    public double minVitamins;
+    public double minLipids;
+    public double maxHealth;
+    public double maxStrength;
+    public double maxSpeed;
+    public double maxLength;
+    public double maxHeight;
+    public double maxKnockback;
+    public double ridingSpeed;
+
+    public float xzBoxMin;
+    public float yBoxMin;
+    public float xzBoxDelta;
+    public float yBoxDelta;
+
+    public int fullGrowthTime;
+    public int timesOfGrowth;
+    public int cultivateSpeed;
+    public int numberOfStages;
+
+    public ArrayList favoriteFoodList;
+    public ArrayList ridingItemList;
+
+    public boolean isRidable;
 }
