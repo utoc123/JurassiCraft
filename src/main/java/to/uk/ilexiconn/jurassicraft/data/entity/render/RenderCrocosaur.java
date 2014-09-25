2c48ce342adb18e669569aa82eeac58a13faa6b8
TheLarsinator <TheLarsinatormodding@hotmail.com>
2014-09-25 21:12:11 +0200

    Crocosaur

---
+++ src/main/java/to/uk/ilexiconn/jurassicraft/data/entity/render/RenderCrocosaur.java
@@ -0,0 +1,33 @@
+package to.uk.ilexiconn.jurassicraft.data.entity.render;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.util.ResourceLocation;
+import to.uk.ilexiconn.jurassicraft.JurassiCraft;
+import to.uk.ilexiconn.jurassicraft.data.entity.Dinosaur;
+import to.uk.ilexiconn.jurassicraft.data.entity.RenderDinosaur;
+import to.uk.ilexiconn.jurassicraft.data.entity.entity.EntityCrocosaur;
+import to.uk.ilexiconn.jurassicraft.data.entity.model.ModelCrocosaur;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+
+@SideOnly(Side.CLIENT)
+public class RenderCrocosaur extends RenderDinosaur
+{
+    public RenderCrocosaur(Dinosaur dino)
+    {
+        super(new ModelCrocosaur(), dino, 0.5F);
+    }
+
+    public ResourceLocation getEntityTexture(Entity var1)
+    {
+        EntityCrocosaur dino = (EntityCrocosaur) var1;
+        switch (dino.getCreatureTexture())
+        {
+            case 1:
+            default:
+                return new ResourceLocation(JurassiCraft.getModId() + "textures/entity/Crocosaur1.png");
+            case 0:
+                return new ResourceLocation(JurassiCraft.getModId() + "textures/entity/Crocosaur2.png");
+        }
+    }
+}
