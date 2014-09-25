1db320445671cec76aafc4d5fa90e114562f113f
Ryan Eastland <ryanjeastland@gmail.com>
2014-09-25 15:08:00 +1000

    Velociraptor only spawns in one skin fixed

--- src/main/java/to/uk/ilexiconn/jurassicraft/data/entity/render/RenderVelociraptor.java
+++ src/main/java/to/uk/ilexiconn/jurassicraft/data/entity/render/RenderVelociraptor.java
@@ -6,6 +6,7 @@ import net.minecraft.util.ResourceLocation;
 import to.uk.ilexiconn.jurassicraft.JurassiCraft;
 import to.uk.ilexiconn.jurassicraft.data.entity.Dinosaur;
 import to.uk.ilexiconn.jurassicraft.data.entity.RenderDinosaur;
+import to.uk.ilexiconn.jurassicraft.data.entity.entity.EntityTyrannosaurus;
 import to.uk.ilexiconn.jurassicraft.data.entity.entity.EntityVelociraptor;
 import to.uk.ilexiconn.jurassicraft.data.entity.model.ModelVelociraptor;
 
@@ -18,14 +19,13 @@ public class RenderVelociraptor extends RenderDinosaur
 
     public ResourceLocation getEntityTexture(Entity var1)
     {
-    	EntityVelociraptor ank = (EntityVelociraptor)var1;
-    	switch(ank.texid)
-    	{
-    	case 0:
-    		default:
-        return new ResourceLocation(JurassiCraft.getModId() + "textures/entity/velociraptor1.png");
-    	case 1:
-        return new ResourceLocation(JurassiCraft.getModId() + "textures/entity/velociraptor2.png");
-    	}
+        EntityVelociraptor dino = (EntityVelociraptor) var1;
+        switch (dino.getCreatureTexture()) {
+            case 1:
+            default:
+                return new ResourceLocation(JurassiCraft.getModId() + "textures/entity/velociraptor1.png");
+            case 0:
+                return new ResourceLocation(JurassiCraft.getModId() + "textures/entity/velociraptor2.png");
+        }
     }
 }
