dfa0a7025d60b311ba772940a6db58524d4b8351
Ryan Eastland <ryanjeastland@gmail.com>
2014-09-27 16:03:10 +1000

    Big DERP from lex

--- src/main/java/to/uk/ilexiconn/jurassicraft/ModCreativeTabs.java
+++ src/main/java/to/uk/ilexiconn/jurassicraft/ModCreativeTabs.java
@@ -1,12 +1,13 @@
 package to.uk.ilexiconn.jurassicraft;
 
+import cpw.mods.fml.common.eventhandler.EventPriority;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import to.uk.ilexiconn.llib.content.ContentHandler;
 import to.uk.ilexiconn.llib.content.ContentType;
 
-@ContentHandler(modid = "jurassicraft", type = ContentType.CREATIVETAB)
+@ContentHandler(modid = "jurassicraft", type = ContentType.CREATIVETAB, priority = EventPriority.HIGHEST)
 public class ModCreativeTabs
 {
     public static CreativeTabs fossilTab;
