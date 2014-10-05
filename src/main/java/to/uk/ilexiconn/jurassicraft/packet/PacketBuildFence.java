6ececfa1ff368ce10f79bb29e9355cbec1f77665
iLexiconn <ilexiconn@gmail.com>
2014-10-05 11:34:52 +0200

    packet stuff

---
+++ src/main/java/to/uk/ilexiconn/jurassicraft/packet/PacketBuildFence.java
@@ -0,0 +1,29 @@
+package to.uk.ilexiconn.jurassicraft.packet;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import net.minecraft.entity.player.EntityPlayer;
+import thehippomaster.AnimationAPI.packet.AbstractPacket;
+
+public class PacketBuildFence extends AbstractPacket
+{
+    public void encodeInto(ChannelHandlerContext ctx, ByteBuf buffer)
+    {
+
+    }
+
+    public void decodeInto(ChannelHandlerContext ctx, ByteBuf buffer)
+    {
+
+    }
+
+    public void handleClientSide(EntityPlayer player)
+    {
+
+    }
+
+    public void handleServerSide(EntityPlayer player)
+    {
+
+    }
+}
