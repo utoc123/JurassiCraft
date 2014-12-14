f26ec9660f0b0e4f4fe45eadba97f708abfe810d
a3626a <a3626a@naver.com>
2014-12-14 16:16:21 +0900

    Movement range expanded
    for the request of kash, I make some changes with ankylosaur. This
    change can be applied to every land dinos.
    (EntityJurassiCraftLandProtective)

--- src/main/java/to/uk/ilexiconn/jurassicraft/ai/JurassiCraftEntityAIWander.java
+++ src/main/java/to/uk/ilexiconn/jurassicraft/ai/JurassiCraftEntityAIWander.java
@@ -2,59 +2,56 @@ package to.uk.ilexiconn.jurassicraft.ai;
 
 import net.minecraft.entity.ai.EntityAIBase;
 import net.minecraft.entity.ai.RandomPositionGenerator;
+import net.minecraft.pathfinding.PathEntity;
+import net.minecraft.pathfinding.PathNavigate;
 import net.minecraft.util.Vec3;
 import to.uk.ilexiconn.jurassicraft.entity.EntityJurassiCraftCreature;
 
-public class JurassiCraftEntityAIWander extends EntityAIBase
-{
-    private EntityJurassiCraftCreature entity;
-    private double xPosition;
-    private double yPosition;
-    private double zPosition;
-    private double speed;
-	private int maxDistance;
-	private int maxHeight;
+public class JurassiCraftEntityAIWander extends EntityAIBase {
+	private EntityJurassiCraftCreature entity;
+	private double xPosition;
+	private double yPosition;
+	private double zPosition;
+	private double speed;
+	private double maxDistance;
+	private double maxHeight;
 
-    public JurassiCraftEntityAIWander(EntityJurassiCraftCreature creature, int distance, int height, double velocity)
-    {
-        this.entity = creature;
-        this.speed = velocity;
-        this.maxDistance = distance;
-        this.maxHeight = height;
-        this.setMutexBits(1);
-    }
-    
-    public boolean shouldExecute()
-    {
-        if (this.entity.getRNG().nextInt(120) != 0)
-        {
-            return false;
-        }
-        else
-        {
-            Vec3 vec3 = RandomPositionGenerator.findRandomTarget(this.entity, this.maxDistance, this.maxHeight);
-            if (vec3 == null)
-            {
-                return false;
-            }
-            else
-            {
-                this.xPosition = vec3.xCoord;
-                this.yPosition = vec3.yCoord;
-                this.zPosition = vec3.zCoord;
-                return true;
-            }
-        }
-    }
-    
-    public void startExecuting()
-    {
-        this.entity.getNavigator().tryMoveToXYZ(this.xPosition, this.yPosition, this.zPosition, this.speed);
-    }
-    
-    public boolean continueExecuting()
-    {
-        return !this.entity.getNavigator().noPath();
-    }
-    
+	public JurassiCraftEntityAIWander(EntityJurassiCraftCreature creature,
+			double distance, double height, double velocity) {
+		this.entity = creature;
+		this.speed = velocity;
+		this.maxDistance = distance;
+		this.maxHeight = height;
+		this.setMutexBits(1);
+	}
+
+	public boolean shouldExecute() {
+		if (this.entity.getRNG().nextInt(120) != 0) {
+			return false;
+		} else {
+			Vec3 vec3 = RandomPositionGenerator.findRandomTarget(this.entity,
+					(int) (this.maxDistance * Math.sqrt(this.entity
+							.getGrowthStage())), 2);
+			//Now searching range is increased as growth stage increase.
+			if (vec3 == null) {
+				return false;
+			} else {
+				this.xPosition = vec3.xCoord;
+				this.yPosition = vec3.yCoord;
+				this.zPosition = vec3.zCoord;
+				return true;
+			}
+		}
+	}
+
+	public void startExecuting() {
+		this.entity.getNavigator().tryMoveToXYZ(this.xPosition, this.yPosition,
+				this.zPosition, this.speed);
+	}
+
+	public boolean continueExecuting() {
+		return !this.entity.getNavigator().noPath();
+
+	}
+
 }
--- src/main/java/to/uk/ilexiconn/jurassicraft/entity/dinosaurs/EntityAnkylosaur.java
+++ src/main/java/to/uk/ilexiconn/jurassicraft/entity/dinosaurs/EntityAnkylosaur.java
@@ -1,5 +1,6 @@
 package to.uk.ilexiconn.jurassicraft.entity.dinosaurs;
 
+import net.minecraft.entity.SharedMonsterAttributes;
 import net.minecraft.entity.ai.EntityAILookIdle;
 import net.minecraft.entity.ai.EntityAISwimming;
 import net.minecraft.entity.ai.EntityAIWatchClosest;
@@ -14,78 +15,84 @@ import to.uk.ilexiconn.jurassicraft.ai.JurassiCraftEntityAIWander;
 import to.uk.ilexiconn.jurassicraft.entity.EntityJurassiCraftLandProtective;
 import to.uk.ilexiconn.jurassicraft.entity.IDinosaur;
 
-public class EntityAnkylosaur extends EntityJurassiCraftLandProtective implements IDinosaur
-{
+public class EntityAnkylosaur extends EntityJurassiCraftLandProtective
+		implements IDinosaur {
 
-    public EntityAnkylosaur(World world)
-    {
-        super(world, (byte) Util.classToId(EntityAnkylosaur.class), 1);
-        this.getNavigator().setAvoidsWater(true);
-        this.tasks.addTask(0, new EntityAISwimming(this));
-        this.tasks.addTask(2, this.aiSit);
-        this.tasks.addTask(4, new JurassiCraftEntityAIFollowFood(this, 1.1D * this.getCreatureSpeed()));
-        this.tasks.addTask(4, new JurassiCraftEntityAIEatDroppedFood(this, 16.0D));
-        this.tasks.addTask(5, new JurassiCraftEntityAIWander(this, 16, 6, 0.7D * this.getCreatureSpeed()));
-        this.tasks.addTask(6, new EntityAIWatchClosest(this, EntityPlayer.class, 6.0F));
-        this.tasks.addTask(6, new EntityAILookIdle(this));
-        this.setCreatureExperiencePoints(5000);
-    }
-
-    @Override
-    public double getMountedYOffset()
-    {
-        return (double) this.getYBouningBox() * 1.05D;
-    }
-
-    @Override
-    public int getTalkInterval()
-    {
-        return 350;
-    }
-
-    @Override
-    public String getLivingSound()
-    {
-        if (this.rand.nextInt(2) == 0)
-        {
-            return Util.getCreatureFromId(this.getCreatureID()).livingSound1;
-        }
-        else
-        {
-            return Util.getCreatureFromId(this.getCreatureID()).livingSound2;
-        }
-    }
-
-    @Override
-    public String getHurtSound()
-    {
-        return Util.getCreatureFromId(this.getCreatureID()).hurtSound;
-    }
-
-    @Override
-    public String getDeathSound()
-    {
-        return Util.getCreatureFromId(this.getCreatureID()).deathSound;
-    }
-
-    @Override
-    public Item getDropItem()
-    {
-        return Util.getMeat(this.getCreatureID());
-    }
+	public EntityAnkylosaur(World world) {
+		super(world, (byte) Util.classToId(EntityAnkylosaur.class), 1);
+		this.getNavigator().setAvoidsWater(true);
+		this.tasks.addTask(0, new EntityAISwimming(this));
+		this.tasks.addTask(2, this.aiSit);
+		this.tasks.addTask(4, new JurassiCraftEntityAIFollowFood(this,
+				1.1D * this.getCreatureSpeed()));
+		this.tasks.addTask(4, new JurassiCraftEntityAIEatDroppedFood(this,
+				16.0D));
+		this.tasks.addTask(5, new JurassiCraftEntityAIWander(this, 16, 6,
+				0.7D * this.getCreatureSpeed()));
+		this.tasks.addTask(6, new EntityAIWatchClosest(this,
+				EntityPlayer.class, 6.0F));
+		this.tasks.addTask(6, new EntityAILookIdle(this));
+		this.setCreatureExperiencePoints(5000);
+	}
 
 	@Override
-	protected void dropFewItems(boolean recentlyBeenHit, int enchantBonus) 
-	{
+	protected void applyEntityAttributes() {
+		super.applyEntityAttributes();
+		this.getAttributeMap().getAttributeInstance(SharedMonsterAttributes.followRange).setBaseValue(256.0D);
+		// this followRange attribute limits max movement range. this is the reason i(oortcloud) expand this value.
+	}
+
+	@Override
+	public double getMountedYOffset() {
+		return (double) this.getYBouningBox() * 1.05D;
+	}
+
+	@Override
+	public int getTalkInterval() {
+		return 350;
+	}
+
+	@Override
+	public String getLivingSound() {
+		if (this.rand.nextInt(2) == 0) {
+			return Util.getCreatureFromId(this.getCreatureID()).livingSound1;
+		} else {
+			return Util.getCreatureFromId(this.getCreatureID()).livingSound2;
+		}
+	}
+
+	@Override
+	public String getHurtSound() {
+		return Util.getCreatureFromId(this.getCreatureID()).hurtSound;
+	}
+
+	@Override
+	public String getDeathSound() {
+		return Util.getCreatureFromId(this.getCreatureID()).deathSound;
+	}
+
+	@Override
+	public Item getDropItem() {
+		return Util.getMeat(this.getCreatureID());
+	}
+
+	@Override
+	protected void dropFewItems(boolean recentlyBeenHit, int enchantBonus) {
 		float developmentFraction = this.getGrowthStage() / 120.0F;
-		int count = Math.round(1 + (3.0F * developmentFraction) + this.rand.nextInt(2 + (int) (3.0F * developmentFraction)) + this.rand.nextInt(1 + enchantBonus));
-		if (this.isBurning()) 
-		{
+		int count = Math.round(1 + (3.0F * developmentFraction)
+				+ this.rand.nextInt(2 + (int) (3.0F * developmentFraction))
+				+ this.rand.nextInt(1 + enchantBonus));
+		if (this.isBurning()) {
 			this.dropItem(ModItems.dinoSteak, count);
-		} 
-		else 
-		{
+		} else {
 			this.dropItem(Util.getMeat(this.getCreatureID()), count);
 		}
+	}
+	
+	@Override
+	public float getBlockPathWeight(int x, int y,
+			int z) {
+		// This make searching target position efficient.
+		return this.worldObj.isAirBlock(x, y-1, z) ? 0.0F : 1.0F;
 	}
 }
